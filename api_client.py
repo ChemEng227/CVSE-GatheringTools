3e75112f515c7b6a8844fe0fe99925c716c37fd7
yhtq <1414672068@qq.com>
2025-11-20 17:51:32 +0800

    update

--- api_client.py
+++ api_client.py
@@ -1,7 +1,8 @@
+import asyncio
 import enum
 import os
 import sys
-from functools import singledispatch
+from datetime import datetime
 from typing import Any, Iterable, TypedDict, TypeVar
 
 import capnp
@@ -18,22 +19,36 @@ class Rank(enum.Enum):
     UTAU = 3
 
 
+class RPCTime:
+    def __init__(self, seconds: int, nanoseconds: int) -> None:
+        self.seconds = seconds
+        self.nanoseconds = nanoseconds
+
+    @staticmethod
+    def from_datetime(dt: datetime) -> "RPCTime":
+        return RPCTime(int(dt.timestamp()), dt.microsecond * 1000)
+
+    def to_datetime(self) -> datetime:
+        return datetime.fromtimestamp(self.seconds + self.nanoseconds / 1e9)
+
+
 class ModifyEntry(TypedDict):
     avid: str
     bvid: str
     ranks: list[Rank] | None
     is_republish: bool | None
     staff: str | None
+    is_examined: bool | None
 
 
-class AddressingNewEntry(TypedDict):
+class RecordingNewEntry(TypedDict):
     avid: str
     bvid: str
     title: str
     uploader: str
     up_face: str
     copyright: int
-    pubdate: str
+    pubdate: RPCTime
     duration: int
     page: int
     cover: str
@@ -41,7 +56,7 @@ class AddressingNewEntry(TypedDict):
     tags: list[str]
 
 
-class AddressingDataEntry(TypedDict):
+class RecordingDataEntry(TypedDict):
     avid: str
     bvid: str
     view: int
@@ -51,7 +66,12 @@ class AddressingDataEntry(TypedDict):
     danmaku: int
     reply: int
     share: int
-    date: dict[str, int]  # e.g., {"year": 2023, "month": 10, "day": 5}
+    date: RPCTime
+
+
+class Index(TypedDict):
+    avid: str
+    bvid: str
 
 
 T = TypeVar("T")
@@ -64,50 +84,71 @@ def build_list_to_capnp(l: Iterable[T], builder) -> None:
     return capnp_list
 
 
-@singledispatch
-def to_capnp(obj: Any) -> None:
-    raise NotImplementedError(f"Unsupported type: {type(obj)}")
+def RPCTime_to_capnp(obj: RPCTime) -> "CVSE_capnp.Cvse.Time":
+    time = CVSE_capnp.Cvse.Time.new_message()
+    time.seconds = obj.seconds
+    time.nanoseconds = obj.nanoseconds
+    return time
 
 
-@to_capnp.register(Rank)
-def _(obj: Rank):
-    rank = CVSE_capnp.Rank.new_message()
-    rank = rank.init("value", 1)[0]
+def capnp_to_RPCTime(obj: "CVSE_capnp.Cvse.Time") -> RPCTime:
+    return RPCTime(obj.seconds, obj.nanoseconds)
+
+
+def datetime_to_capnp(obj: datetime) -> "CVSE_capnp.Cvse.Time":
+    return RPCTime_to_capnp(RPCTime.from_datetime(obj))
+
+
+def Rank_to_capnp(obj: Rank) -> "CVSE_capnp.Cvse.Rank":
+    rank = CVSE_capnp.Cvse.Rank.new_message()
     match obj:
         case Rank.DOMESTIC:
-            rank.type = "domestic"
+            rank.value = "domestic"
         case Rank.SV:
-            rank.type = "sv"
+            rank.value = "sv"
         case Rank.UTAU:
-            rank.type = "utau"
+            rank.value = "utau"
     return rank
 
 
-@to_capnp.register(ModifyEntry)
-def _(obj: ModifyEntry):
-    entry = CVSE_capnp.ModifyEntry.new_message()
-    entry.avid = int(obj["avid"])
+def ModifyEntry_to_capnp(obj: ModifyEntry) -> "CVSE_capnp.Cvse.ModifyEntry":
+    entry = CVSE_capnp.Cvse.ModifyEntry.new_message()
+    entry.avid = obj["avid"]
     entry.bvid = obj["bvid"]
+    entry.hasRanks = obj["ranks"] is not None
+    entry.hasStaffInfo = obj["staff"] is not None
+    entry.hasIsExamined = obj["is_examined"] is not None
     if obj["ranks"] is not None:
-        ranks = map(to_capnp, obj["ranks"])
+        ranks = map(Rank_to_capnp, obj["ranks"])
         build_list_to_capnp(ranks, entry.init("ranks", len(obj["ranks"])))
+    else:
+        entry.init("ranks", 0)
     if obj["is_republish"] is not None:
         entry.isRepublish = obj["is_republish"]
+    else:
+        entry.isRepublish = False
     if obj["staff"] is not None:
-        entry.staff = obj["staff"]
+        entry.staffInfo = obj["staff"]
+    else:
+        entry.staffInfo = ""
+    if obj["is_examined"] is not None:
+        entry.isExamined = obj["is_examined"]
+    else:
+        entry.isExamined = False
     return entry
 
 
-@to_capnp.register(AddressingNewEntry)
-def _(obj: AddressingNewEntry):
-    entry = CVSE_capnp.AddressingNewEntry.new_message()
+def RecordingNewEntry_to_capnp(
+    obj: RecordingNewEntry,
+) -> "CVSE_capnp.Cvse.RecordingNewEntry":
+    entry = CVSE_capnp.Cvse.RecordingNewEntry.new_message()
     entry.avid = obj["avid"]
     entry.bvid = obj["bvid"]
     entry.title = obj["title"]
     entry.uploader = obj["uploader"]
     entry.upFace = obj["up_face"]
     entry.copyright = obj["copyright"]
-    entry.pubdate = obj["pubdate"]
+    entry.pubdate = RPCTime_to_capnp(obj["pubdate"])
     entry.duration = obj["duration"]
     entry.page = obj["page"]
     entry.cover = obj["cover"]
@@ -116,9 +157,10 @@ def _(obj: AddressingNewEntry):
     return entry
 
 
-@to_capnp.register(AddressingDataEntry)
-def _(obj: AddressingDataEntry):
-    entry = CVSE_capnp.AddressingDataEntry.new_message()
+def RecordingDataEntry_to_capnp(
+    obj: RecordingDataEntry,
+) -> "CVSE_capnp.Cvse.RecordingDataEntry":
+    entry = CVSE_capnp.Cvse.RecordingDataEntry.new_message()
     entry.avid = obj["avid"]
     entry.bvid = obj["bvid"]
     entry.view = obj["view"]
@@ -128,42 +170,213 @@ def _(obj: AddressingDataEntry):
     entry.danmaku = obj["danmaku"]
     entry.reply = obj["reply"]
     entry.share = obj["share"]
-    date = entry.date
-    date.year = obj["date"]["year"]
-    date.month = obj["date"]["month"]
-    date.day = obj["date"]["day"]
+    entry.date = RPCTime_to_capnp(obj["date"])
     return entry
 
 
+def Index_to_capnp(obj: Index) -> "CVSE_capnp.Cvse.Index":
+    index = CVSE_capnp.Cvse.Index.new_message()
+    index.avid = obj["avid"]
+    index.bvid = obj["bvid"]
+    return index
+
+
 class CVSE_Client:
-    def __init__(self, connection):
+    def __init__(self, connection, client, cvse):
         self.connection = connection
-        self.client = capnp.TwoPartyClient(connection)
-        self.cvse = self.client.bootstrap().cast_as(CVSE_capnp.CVSE)
+        self.client = client
+        self.cvse = cvse
 
-    async def updateModifyEntry(self, entries: list[ModifyEntry]) -> None:
-        size = len(entries)
-        entries_capnp = map(to_capnp, entries)
-        request = self.cvse.updateModifyEntry_request()
-        build_list_to_capnp(entries_capnp, request.init("entries", size))
-        await request.send()
+    @staticmethod
+    async def create(host, port) -> "CVSE_Client":
+        connection = await capnp.AsyncIoStream.create_connection(host=host, port=port)
+        client = capnp.TwoPartyClient(connection)
+        cvse = client.bootstrap().cast_as(CVSE_capnp.Cvse)
+        self = CVSE_Client(connection, client, cvse)
+        return self
 
-    async def updateNewEntry(self, entries: list[AddressingNewEntry]) -> None:
-        size = len(entries)
-        entries_capnp = map(to_capnp, entries)
-        request = self.cvse.updateNewEntry_request()
-        build_list_to_capnp(entries_capnp, request.init("entries", size))
-        await request.send()
+    # 所有函数的参数和返回值都使用序列化格式
+    # （也就是类型标注中的 CVSE_capnp.Cvse.xxx）
+    # 内存效率比 Python 原生格式更高
+    # 具体 field 参考 CVSE-API/CVSE.capnp
 
-    async def updateAddressingDataEntry(
-        self, entries: list[AddressingDataEntry]
+    async def updateModifyEntry(
+        self, entries: list["CVSE_capnp.Cvse.ModifyEntry"]
     ) -> None:
         size = len(entries)
-        entries_capnp = map(to_capnp, entries)
-        request = self.cvse.updateAddressingDataEntry_request()
-        build_list_to_capnp(entries_capnp, request.init("entries", size))
+        request = self.cvse.updateModifyEntry_request()
+        build_list_to_capnp(entries, request.init("entries", size))
         await request.send()
 
+    async def updateNewEntry(
+        self, entries: list["CVSE_capnp.Cvse.RecordingNewEntry"]
+    ) -> None:
+        size = len(entries)
+        request = self.cvse.updateNewEntry_request()
+        build_list_to_capnp(entries, request.init("entries", size))
+        await request.send()
 
-def main():
-    print("This is a test function in utils.py")
+    async def updateRecordingDataEntry(
+        self, entries: list["CVSE_capnp.Cvse.RecordingDataEntry"]
+    ) -> None:
+        size = len(entries)
+        request = self.cvse.updateRecordingDataEntry_request()
+        build_list_to_capnp(entries, request.init("entries", size))
+        await request.send()
+
+    #
+    # 获取所有项的索引
+    # 若 get_unexamined 为 True，则返回值包含未经过收录的索引
+    # 若 get_unincluded 为 True，则返回值包含未收录在任何刊的索引
+    async def getAll(
+        self, get_unexamined: bool, get_unincluded: bool
+    ) -> list["CVSE_capnp.Cvse.Index"]:
+        request = self.cvse.getAll_request()
+        request.get_unexamined = get_unexamined
+        request.get_unincluded = get_unincluded
+        response = await request.send()
+        return response.indices
+
+    async def lookupMetaInfo(
+        self,
+        indices: list["CVSE_capnp.Cvse.Index"],
+    ) -> list["CVSE_capnp.Cvse.RecordingNewEntry"]:
+        request = self.cvse.lookupMetaInfo_request()
+        build_list_to_capnp(indices, request.init("indices", len(indices)))
+        response = await request.send()
+        return response.entries
+
+    async def lookupDataInfo(
+        self,
+        indices: list["CVSE_capnp.Cvse.Index"],
+        from_date: RPCTime,
+        to_date: RPCTime,
+    ) -> list[list["CVSE_capnp.Cvse.RecordingDataEntry"]]:
+        request = self.cvse.lookupDataInfo_request()
+        build_list_to_capnp(indices, request.init("indices", len(indices)))
+        request.from_date = RPCTime_to_capnp(from_date)
+        request.to_date = RPCTime_to_capnp(to_date)
+        response = await request.send()
+        return response.entries
+
+    async def lookupOneDataInfo(
+        self,
+        indices: list[CVSE_capnp.Cvse.Index],
+        from_date: RPCTime,
+        to_date: RPCTime,
+    ) -> list["CVSE_capnp.Cvse.RecordingDataEntry"]:
+        request = self.cvse.lookupOneDataInfo_request()
+        build_list_to_capnp(indices, request.init("indices", len(indices)))
+        request.from_date = RPCTime_to_capnp(from_date)
+        request.to_date = RPCTime_to_capnp(to_date)
+        response = await request.send()
+        return response.entries
+
+
+async def __test() -> None:
+    client = await CVSE_Client.create("47.104.152.246", "8663")
+    test_new_entries: list[RecordingNewEntry] = [
+        {
+            "avid": "av1",
+            "bvid": "bv1",
+            "title": "title1",
+            "desc": "description1",
+            "tags": ["tag1", "tag2"],
+            "cover": "cover1",
+            "duration": 100,
+            "uploader": "uploader1",
+            "up_face": "face1",
+            "copyright": 1,
+            "pubdate": RPCTime.from_datetime(datetime.now()),
+            "page": 1,
+        },
+        {
+            "avid": "av2",
+            "bvid": "bv2",
+            "title": "title2",
+            "desc": "description2",
+            "tags": ["tag3", "tag4"],
+            "cover": "cover2",
+            "duration": 200,
+            "uploader": "uploader2",
+            "up_face": "face2",
+            "copyright": 2,
+            "pubdate": RPCTime.from_datetime(datetime.now()),
+            "page": 2,
+        },
+    ]
+    test_data_entries: list[RecordingDataEntry] = [
+        {
+            "avid": "av1",
+            "bvid": "bv1",
+            "view": 100,
+            "danmaku": 50,
+            "reply": 20,
+            "favorite": 30,
+            "coin": 40,
+            "share": 50,
+            "like": 60,
+            "date": RPCTime.from_datetime(datetime.now()),
+        },
+        {
+            "avid": "av2",
+            "bvid": "bv2",
+            "view": 200,
+            "danmaku": 100,
+            "reply": 40,
+            "favorite": 60,
+            "coin": 80,
+            "share": 100,
+            "like": 120,
+            "date": RPCTime.from_datetime(datetime.now()),
+        },
+    ]
+    test_modify_entries: list[ModifyEntry] = [
+        {
+            "avid": "av1",
+            "bvid": "bv1",
+            "is_republish": True,
+            "ranks": [],
+            "staff": "sss",
+            "is_examined": True,
+        },
+        {
+            "avid": "av2",
+            "bvid": "bv2",
+            "is_republish": False,
+            "ranks": [Rank.DOMESTIC],
+            "staff": None,
+            "is_examined": False,
+        },
+    ]
+    test_new_entries1 = list(map(RecordingNewEntry_to_capnp, test_new_entries))
+    # await client.updateNewEntry(test_new_entries1)
+    test_data_entries1 = list(map(RecordingDataEntry_to_capnp, test_data_entries))
+    test_data_entries2 = test_data_entries1 * 100
+    test_modify_entries1 = list(map(ModifyEntry_to_capnp, test_modify_entries))
+    task1 = asyncio.create_task(client.updateRecordingDataEntry(test_data_entries2))
+    task2 = asyncio.create_task(client.updateModifyEntry(test_modify_entries1))
+    await asyncio.gather(task1, task2)
+    all_info = await client.getAll(True, True)
+    all_meta_info = await client.lookupMetaInfo(all_info)
+    for index in all_info:
+        print(f"avid: {index.avid}, bvid: {index.bvid}")
+    for meta_info in all_meta_info:
+        print(
+            f"avid: {meta_info.avid}, bvid: {meta_info.bvid}, title: {meta_info.title}"
+        )
+    rpc_time_zero: RPCTime = RPCTime(0, 0)
+    all_data = await client.lookupDataInfo(
+        all_info, rpc_time_zero, RPCTime.from_datetime(datetime.now())
+    )
+    for datas in all_data:
+        for data in datas:
+            print(
+                f"{data.bvid}: {data.view} at {capnp_to_RPCTime(data.date).to_datetime()}"
+            )
+
+    return
+
+
+def test():
+    asyncio.run(capnp.run(__test()))
