9235816a76e86932022b4edae47e8dd9c75a70a9
yhtq <1414672068@qq.com>
2025-11-27 15:53:36 +0800

    update

--- api_client.py
+++ api_client.py
@@ -425,6 +425,7 @@ class CVSE_Client:
 
 
 async def __test() -> None:
+    # 仅作用法示例，不要运行，防止向数据库中加入无用数据
     client = await CVSE_Client.create("47.104.152.246", "8663")
     test_new_entries: list[RecordingNewEntry] = [
         {
